293af557ea007939498363035276162e6008c992
iman91 <inaslpak@yahoo.com>
2017-03-10 19:01:37 +1000

    Add files via upload

---
+++ Iman Naslpak/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPLexTutorial
+{
+    public enum Tokens
+    {
+        stringliteral = 300,
+        BooleanLiteral = 256,
+        EOF = 264
+    };
+
+    public struct MyValueType
+    {
+        public int num;
+        public string name;
+    };
+
+    public abstract class ScanBase
+    {
+        public MyValueType yylval;
+        public abstract int yylex();
+        protected virtual bool yywrap() { return true; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scanner scanner = new Scanner(
+    new System.IO.FileStream(args[0], System.IO.FileMode.Open));
+
+
+            Tokens token;
+            do
+            {
+                token = (Tokens)scanner.yylex();
+                switch (token)
+                {
+                    case Tokens.stringliteral:
+                       // scanner.yylval.name = scanner.yylval.name.Replace("\\","");              
+                        Console.WriteLine("stringliteral ({0})", scanner.yylval.name);
+                        break;
+                    case Tokens.BooleanLiteral:
+                        Console.WriteLine("BooleanLiteral ({0})", scanner.yylval.name);
+                        break;
+                    case Tokens.EOF:
+                        Console.WriteLine("EOF");
+                        break;
+                    default:
+                        Console.WriteLine("'{0}'", (char)token);
+                        break;
+                }
+            }
+            while (token != Tokens.EOF);
+
+        }
+    }
+}
+
--- Iman Naslpak/scanner.lex
+++ Iman Naslpak/scanner.lex
@@ -1,1 +1,15 @@
+%namespace GPLexTutorial
 
+escapecharacter [b, t, n, f, r, ", ', \\, 0, 1, 2, 3, 4, 5, 6,7]
+
+%%
+true|false		{yylval.name = yytext; return (int)Tokens.BooleanLiteral;}
+\"((\\{escapecharacter})|[^\\"])*\"	{yylval.name = yytext.Replace("\\\"","\""); return (int)Tokens.stringliteral; }
+[ \r\n\t]                    /* skip whitespace */
+
+.                            { 
+                                 throw new Exception(
+                                     String.Format(
+                                         "unexpected character '{0}'", yytext)); 
+                             }0
+%%

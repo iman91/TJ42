e7f85b765e4e18f4ceecb0026fa6fab8ee435fdc
n9546839 <czzechen@gmail.com>
2017-03-19 11:21:40 +1000

    Add files via upload

---
+++ Ze Chen/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPLexTutorial
+{
+    public struct MyValueType
+    {
+        public string num;
+        public string flo;
+    };
+
+    public enum Tokens
+    {
+        IntergerLiteral,
+        FloatingPointLiteral,
+        EOF
+    };
+
+    public abstract class ScanBase
+    {
+        public MyValueType yylval;
+        public abstract int yylex();
+        protected virtual bool yywrap() { return true; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                using (var input = System.IO.File.OpenRead(args[0]))
+                {
+                    Scanner scanner = new Scanner(input);
+
+                    Tokens token;
+                    do
+                    {
+                        token = (Tokens)scanner.yylex();
+                        switch (token)
+                        {
+                            case Tokens.IntergerLiteral:
+                                Console.WriteLine("IntergerLiteral ({0})", scanner.yylval.num);
+                                break;
+                            case Tokens.FloatingPointLiteral:
+                                Console.WriteLine("FloatingPointLiteral ({0})", scanner.yylval.flo);
+                                break;
+                            case Tokens.EOF:
+                                Console.WriteLine("EOF");
+                                break;
+                            default:
+                                Console.WriteLine("'{0}'", (char)token);
+                                break;
+                        }
+                    }
+                    while (token != Tokens.EOF);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- Ze Chen/scanner.lex
+++ Ze Chen/scanner.lex
@@ -1,12 +1,22 @@
 %namespace GPLexTutorial
-digit [0-9]
-float [0-9]+\.[0-9]+
+
+DecimalNumeral					[0-9_]+[lL]?
+OctalNumeral					0[0-7_]+[lL]?
+HexNumeral						0[xX][0-9a-fA-F_]+[lL]?
+BinaryNumeral					0[bB][0-1_]+[lL]?
+DecimalFloatingPointNumeral			[0-9]*\.?[0-9]+([eE][+-]?[0-9]+)?[dDfF]?
+HexdecimalFloatingPointNumeral			0[xX][0-9a-fA-F]*\.?[0-9a-fA-F]*([pP][+-]?[0-9]+)?
 
 %%
-/* Literals(Integer, FloatingPoint) by Ze Chen (JASON) */
-{digit}+	                 { yylval.num = int.Parse(yytext); return (int)Tokens.INTERGER; }
-{float}+					         { yylval.flo = float.Parse(yytext); return (int)Tokens.FLOAT;}
 
-[ \r\n\t]						/* ingore the white space */ 
+/* Literals(Integer, FloatingPoint) by Ze Chen */
+{DecimalNumeral}					{ yylval.num = yytext; return (int)Tokens.IntergerLiteral; }
+{HexNumeral}						{ yylval.num = yytext; return (int)Tokens.IntergerLiteral; }
+{OctalNumeral}						{ yylval.num = yytext; return (int)Tokens.IntergerLiteral; }
+{BinaryNumeral}						{ yylval.num = yytext; return (int)Tokens.IntergerLiteral; }
 
+{DecimalFloatingPointNumeral}+		{ yylval.flo = yytext; return (int)Tokens.FloatingPointLiteral;}
+{HexdecimalFloatingPointNumeral}	{ yylval.flo = yytext; return (int)Tokens.FloatingPointLiteral;}
+
+.									{}
 %%

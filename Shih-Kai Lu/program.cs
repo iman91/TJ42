726941d6c03f49473f6eba1033739ac79c743175
lusky601632 <lusky99406064@gmail.com>
2017-05-07 16:28:37 +1000

    Update program.cs

--- Shih-Kai Lu/program.cs
+++ Shih-Kai Lu/program.cs
@@ -1,14 +1,15 @@
 using System.IO;
 using System;
 
-namespace GPLexTutorial
+namespace GPLex
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Scanner scanner = new Scanner(
-                new FileStream(args[0], FileMode.Open));
+            var filename = args[0];
+            var file = new FileStream(args[0], FileMode.Open);
+            Scanner scanner = new Scanner(file);
             Parser parser = new Parser(scanner);
             parser.Parse();
             Console.ReadKey();

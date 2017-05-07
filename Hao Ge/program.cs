f6097e7beb470cc0f6d611df0d9dad9946653f0c
GaryGe285648216 <gary285648216@gmail.com>
2017-05-07 16:30:00 +1000

    Update program.cs

--- Hao Ge/program.cs
+++ Hao Ge/program.cs
@@ -1,68 +1,18 @@
+using System.IO;
 using System;
 
-namespace GPLexTutorial
+namespace GPLex
 {
-    public struct MyValueType
-    {
-        public string oper;
-        public string separ;
-    };
-
-    public enum Tokens
-    {
-        Operators,
-        Separators,
-        EOF
-    };
-
-    public abstract class ScanBase
-    {
-        public MyValueType yylval;
-        public abstract int yylex();
-        protected virtual bool yywrap() { return true; }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            
-            try
-            {
-
-                using (var input = System.IO.File.OpenRead(args[0]))
-                {
-                    Scanner scanner = new Scanner(input);
-
-                    Tokens token;
-                    do
-                    {
-                        token = (Tokens)scanner.yylex();
-                        switch (token)
-                        {
-                            case Tokens.Operators:
-                                Console.WriteLine("'{0}'", scanner.yylval.oper);
-                                break;
-                            case Tokens.Separators:
-                                Console.WriteLine("'{0}'", scanner.yylval.separ);
-                                break;
-                            case Tokens.EOF:
-                                Console.WriteLine("EOF");
-                                break;
-                            default:
-                                Console.WriteLine("'{0}'", (char)token);
-                                break;
-                        }
-                    }
-                    while (token != Tokens.EOF);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            var filename = args[0];
+            var file = new FileStream(args[0], FileMode.Open);
+            Scanner scanner = new Scanner(file);
+            Parser parser = new Parser(scanner);
+            parser.Parse();
             Console.ReadKey();
         }
     }
 }
-
